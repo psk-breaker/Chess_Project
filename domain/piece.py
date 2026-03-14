591f782dcb481a87036c855012d794961219a2c1
Pawandeep <pawan9209@gmail.com>
2026-03-14 13:51:27 +0000

    Build basic pawn class with representation and str method, and tests for both. Passing

--- domain/piece.py
+++ domain/piece.py
@@ -2,6 +2,11 @@ class Piece:
 
 
     def __init__(self, colour, piece_type):
-        self.colour = 'none'
-        self.piece_type = 'none'
+        self.colour = colour
+        self.piece_type = piece_type
 
+    def __str__(self):
+        return f'{self.colour} {self.piece_type}'
+
+    def __repr__(self):
+        return f'Piece({self.colour!r}, {self.piece_type!r})'
--- tests/test_piece.py
+++ tests/test_piece.py
@@ -4,8 +4,12 @@ from domain.piece import Piece
 def test_piece_initialisation():
     pawn = Piece('white', 'pawn')
     assert pawn.colour == 'white'
-    assert pawn.name == 'pawn'
+    assert pawn.piece_type == 'pawn'
 
 def test_piece_representation():
+    pawn = Piece('black', 'pawn')
+    assert repr(pawn) == "Piece('black', 'pawn')"
+
+def test_piece_str():
     pawn = Piece('black', 'pawn')
     assert str(pawn) == 'black pawn'
