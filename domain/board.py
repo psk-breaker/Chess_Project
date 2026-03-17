f911c933e3773a88592b2b3e5a4a0d91c3d26fcc
Pawandeep <pawan9209@gmail.com>
2026-03-17 16:02:50 +0000

    feat: Build function for retrieving contents on a given square

--- domain/board.py
+++ domain/board.py
@@ -16,3 +16,8 @@ class Board:
         rank = -int(piece.location[1])
         file = FILE.index(piece.location[0])
         self.grid[rank][file] = piece
+
+    def get_piece(self, square):
+        rank = -int(square[1])
+        file = FILE.index(square[0])
+        return self.grid[rank][file]
