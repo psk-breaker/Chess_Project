1cfca4381f0804796140d8aa3d219c706efe9e46
Pawandeep <pawan9209@gmail.com>
2026-03-18 23:04:07 +0000

    Build test for turn alternation

--- tests/test_game.py
+++ tests/test_game.py
@@ -11,3 +11,12 @@ def test_pawn_game_creation():
     game.pawn_game_creation()
     assert game.board.get_piece('a2').colour == 'white'
     assert game.board.get_piece('c7').colour == 'black'
+
+def test_turn_alternation():
+    game = Game()
+    game.pawn_game_creation()
+    with pytest.raises(ValueError):
+        game.move_piece(game.bp1, 'a6')
+    game.move_piece(game.wp1, 'a4')
+    with pytest.raises(ValueError):
+        game.move_piece(game.wp1, 'a5')
