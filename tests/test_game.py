f8c6859d7acc0834a2d3bc8ead215d123580c8fa
Pawandeep <pawan9209@gmail.com>
2026-03-18 13:32:30 +0000

    Build test for a pawns-only game - this is a secondary game-type offering for the project

--- tests/test_game.py
+++ tests/test_game.py
@@ -4,5 +4,10 @@ from domain.game import Game
 def test_game_creation():
     game = Game()
     assert game.turn == 'white'
-    assert len(game.board) == 8
+    assert len(game.board.grid) == 8
 
+def test_pawn_game_creation():
+    game = Game()
+    game.pawn_game_creation()
+    assert game.board.get_piece('a2').colour == 'white'
+    assert game.board.get_piece('c7').colour == 'black'
