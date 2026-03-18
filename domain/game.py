9d4c7fb6ecc9fa7478b7cfd163a6545bde942165
Pawandeep <pawan9209@gmail.com>
2026-03-18 13:33:34 +0000

    feat: Build pawns-only game as a seecondary offering for the chess learning platform

--- domain/game.py
+++ domain/game.py
@@ -6,3 +6,16 @@ class Game:
         self.turn = 'white'
         self.board = Board()
 
+    def pawn_game_creation(self):
+        wp1 = Pawn('white', 'active', 'a2')
+        wp2 = Pawn('white', 'active', 'b2')
+        wp3 = Pawn('white', 'active', 'c2')
+        self.board.place_piece(wp1)
+        self.board.place_piece(wp2)
+        self.board.place_piece(wp3)
+        bp1 = Pawn('black', 'active', 'a7')
+        bp2 = Pawn('black', 'active', 'b7')
+        bp3 = Pawn('black', 'active', 'c7')
+        self.board.place_piece(bp1)
+        self.board.place_piece(bp2)
+        self.board.place_piece(bp3)
