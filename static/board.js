76b490e9398e6026d3750ce9beccf467be49780e
Pawandeep <pawan9209@gmail.com>
2026-03-23 13:47:24 +0000

    feat: Build legal move square highlight feature, including translation from chess notation to board notation

--- static/board.js
+++ static/board.js
@@ -1,7 +1,10 @@
 PIECES = {
     '': '', 'wp': "♙", 'bp':"♟",
 };
+FILE = 'abcdefgh';
 let selected = null;
+let availableSquares = [];
+let greensq = null;
 
 
 async function fetchBoard() {
@@ -9,6 +12,7 @@ async function fetchBoard() {
     const board = await res.json();
     renderBoard(board);
 }
+
 
 function renderBoard(board) {
     const container = document.getElementById('board');
@@ -30,36 +34,59 @@ function renderBoard(board) {
             }
 
             square.innerText = PIECES[cell] || '';
+            
             square.onclick = () => {
-                handleClick(i, j);
+                handleClick(i, j).then(data => {
+                    console.log("Is there any greens data?:", data);
+                    data.forEach((note) => {
+                        availableSquares.push(toBoardNotation(note));   
+                    });
+                console.log("available squares to higlight:", availableSquares)
+                availableSquares.forEach((square) => {
+                    let i = square[0];
+                    let j = square[1];
+                    const targetSquare = container.children[i].children[j];
+                    targetSquare.classList.add("available")
+                availableSquares = []
+                });                    
+                });
                 square.classList.add("selected");
-            }
+            };
 
             rowDiv.appendChild(square);
         });
 
         container.appendChild(rowDiv);
     });
-}
+
+
+};
+
 
 function toChessNotation(i, j) {
-    const file = String.fromCharCode(97 + j);
-    const rank = 8 - i;
+    let file = String.fromCharCode(97 + j);
+    let rank = 8 - i;
     return file + rank;
 }
 
+function toBoardNotation(notation) {
+    let i = -(parseInt(notation[1]) - 8);
+    let j = FILE.indexOf(notation[0]);
+    return [i, j];
+}
 
 
 async function handleClick(i, j) {
     if (!selected) {
         selected = [i, j];
-        return;
+        const greens = await legalSquares(toChessNotation(i, j));
+        console.log("legalMoves produces greens:", greens)
+        return greens;
     }
 
     const start = toChessNotation(selected[0], selected[1]);
     const end = toChessNotation(i, j);
-    console.log("Move:", start, end);
-
+    
     const res = await fetch("/move", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -71,6 +98,26 @@ async function handleClick(i, j) {
 
     selected = null;
     fetchBoard();
+    return ["a1"];
 }
+
+
+async function legalSquares(selected_square) {
+    const res = await fetch("/legal", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            sq: selected_square
+        })
+    });
+    const availableMoves = await res.json();
+    console.log("AvailableMoves:", availableMoves);
+    return availableMoves;
+};
+
+async function greens(i ,j) {
+    const greens = await handleClick(i, j);
+    return greens;
+};
 
 fetchBoard();
