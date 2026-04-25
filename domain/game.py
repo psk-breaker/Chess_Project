3a85dcde371247c535a0608598f0696b65d8c999
Pawandeep <pawan9209@gmail.com>
2026-04-25 21:33:23 +0100

    Feature: Refactor game creation code. Build bishop game. Build game to test knights: general test game.

--- domain/game.py
+++ domain/game.py
@@ -1,5 +1,5 @@
 from domain.board import Board
-from domain.piece import Pawn, Queen
+from domain.piece import Pawn, Knight, Bishop, Queen
 
 TURN = ['BLANK', 'white', 'black']
 class Game:
@@ -12,36 +12,64 @@ class Game:
         self.wp1 = Pawn('white', 'active', 'a2')
         self.wp2 = Pawn('white', 'active', 'b2')
         self.wp3 = Pawn('white', 'active', 'c2')
-        self.bp1 = Pawn('black', 'active', 'a7')
-        self.bp2 = Pawn('black', 'active', 'b7')
-        self.bp3 = Pawn('black', 'active', 'c7')
-        self.board.place_piece(self.wp1)
-        self.board.place_piece(self.wp2)
-        self.board.place_piece(self.wp3)
-        self.board.place_piece(self.bp1)
-        self.board.place_piece(self.bp2)
-        self.board.place_piece(self.bp3)
-    
-    def queen_game_creation(self):
         self.wp6 = Pawn('white', 'active', 'f2')
         self.wp7 = Pawn('white', 'active', 'g2')
         self.wp8 = Pawn('white', 'active', 'h2')
-        self.wq = Queen('white', 'active', 'd1')
 
+        
+        self.bp1 = Pawn('black', 'active', 'a7')
+        self.bp2 = Pawn('black', 'active', 'b7')
+        self.bp3 = Pawn('black', 'active', 'c7')
         self.bp6 = Pawn('black', 'active', 'f7')
         self.bp7 = Pawn('black', 'active', 'g7')
         self.bp8 = Pawn('black', 'active', 'h7')
-        self.bq = Queen('black', 'active', 'd8')
-
+        self.board.place_piece(self.wp1)
+        self.board.place_piece(self.wp2)
+        self.board.place_piece(self.wp3)
         self.board.place_piece(self.wp6)
         self.board.place_piece(self.wp7)
         self.board.place_piece(self.wp8)
-        self.board.place_piece(self.wq)
-
+        self.board.place_piece(self.bp1)
+        self.board.place_piece(self.bp2)
+        self.board.place_piece(self.bp3)
         self.board.place_piece(self.bp6)
         self.board.place_piece(self.bp7)
-        self.board.place_piece(self.bp8) 
+        self.board.place_piece(self.bp8)
+    
+    def queen_game_creation(self):
+        
+        self.wq = Queen('white', 'active', 'e4')
+        self.bq = Queen('black', 'active', 'd4')
+
+        self.board.place_piece(self.wq) 
         self.board.place_piece(self.bq)
+    
+    def bishop_game_creation(self):
+        self.bb1 = Bishop('black', 'active', 'c8')
+        self.bb2 = Bishop('black', 'active', 'f8')
+        self.wb1 = Bishop('white', 'active', 'c1')
+        self.wb2 = Bishop('white', 'active', 'f1')
+        self.board.place_piece(self.bb1)
+        self.board.place_piece(self.bb2)
+        self.board.place_piece(self.wb1)
+        self.board.place_piece(self.wb2)
+    
+    def knight_game_creation(self):
+        self.wn1 = Knight('white', 'active', 'b1')
+        self.wn2 = Knight('white', 'active', 'f1')
+        self.bn1 = Knight('black', 'active', 'b8')
+        self.bn2 = Knight('black', 'active', 'f8')
+
+        self.board.place_piece(self.wn1)
+        self.board.place_piece(self.wn2)
+        self.board.place_piece(self.bn1)
+        self.board.place_piece(self.bn2)
+
+    def test_game_creation(self):
+        self.pawn_game_creation()
+        self.queen_game_creation()
+        self.bishop_game_creation()
+        self.knight_game_creation()
 
     
     def move_piece(self, piece, target):
